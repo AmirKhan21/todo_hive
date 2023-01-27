bbf76c0a7c75ff5469d1534262081256fe993c07
AmirKhan21 <amo.kh021@gmail.com>
2023-01-27 17:29:03 +0500

    will continue it soonn

---
+++ todo_hive/lib/screens/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('Todo List'), centerTitle: true,),
+        
+      floatingActionButton: FloatingActionButton(onPressed: () {
+       
+      },
+      child: const Icon(Icons.add),
+      ),
+      
+    );
+  }
+}
