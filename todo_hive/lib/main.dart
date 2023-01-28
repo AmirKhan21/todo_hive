904307c1f503f7e98c1a98938db8542dbedc00e7
AmirKhan21 <amo.kh021@gmail.com>
2023-01-28 18:56:55 +0500

    Completed

--- todo_hive/lib/main.dart
+++ todo_hive/lib/main.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:todo_hive/screens/home_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+import 'screens/home_screen.dart';
+
+
+void main() async {
+  // init the hive
+  await Hive.initFlutter();
+
+  // open a box
+  // ignore: unused_local_variable
+  var box = await Hive.openBox('mybox');
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        primarySwatch: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
       home: const HomePage(),
+      theme: ThemeData(primarySwatch: Colors.blue),
     );
   }
 }
-
- 
